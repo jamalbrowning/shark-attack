280ddb9d5c13e40e6bcc103f84c5a673515a0f3f
Jim Browning <jamalebrowning@gmail.com>
2020-08-11 21:31:18 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+
+import studentData from '../helpers/data/studentData';
+
 import './App.scss';
 
 class App extends React.Component {
+  state = {
+    students: [],
+  }
+
+  componentDidMount() {
+    const students = studentData.getStudents();
+    this.setState({ students });
+  }
+
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        <h1>The Shark Tank</h1>
+        <button className="btn btn-info">Shark Attack</button>
       </div>
     );
   }
